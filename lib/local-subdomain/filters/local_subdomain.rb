ad9097f849d443783009e5f94c8323ea9de0f83d
ahumbert <a.humbert@tukaiz.com>
2024-05-01 15:50:15 -0500

    Adjusting for Docker Network

--- lib/local-subdomain/filters/local_subdomain.rb
+++ lib/local-subdomain/filters/local_subdomain.rb
@@ -8,6 +8,12 @@ module LocalSubdomain
   def redirect_to_lvh_me
     return unless Rails.env.development?
 
+    #- Docker issues the random/next-available ip on instance create and assigns the ip to the
+    #     external net as a hostname (of the instance's name)
+    #- TC is calling for BS2 by this hostname
+    #- Below is blocking this redirect, which breaks when TC is calling
+    return unless ENV['host'] == 'backstage-2'
+
     redirect_domain = ENV['SERVER_REDIRECT_DOMAIN'] || 'lvh.me'
 
     served_by_lvh_me = !request.env['SERVER_NAME'][/#{redirect_domain}$/].nil?
